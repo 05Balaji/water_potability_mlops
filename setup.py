2c498680b12d28ed5b4844d5e7f46601f6fda6ce
Sibichakravarthi <sibi46192@gmil.com>
2026-06-23 08:40:11 +0530

    additional readme details added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "water_protability_mlops",
+    version = "0.0.0",
+    author = "Balaji",
+    author_email = "balajimurugan385@gmail.com",
+    packages = find_packages(),
+)
